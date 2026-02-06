8646c0d9fe45fe78ffebbce465b86cf67328fa72
ameniissaoui <ameni.issaoui@esprit.tn>
2026-02-06 16:16:59 +0000

    workshop3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Pour ngModel
+import { CommonModule } from '@angular/common'; // Pour ngClass, pipes
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './core/home/home.component';
+import { NotfoundComponent } from './core/notfound/notfound.component';
 import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.component';
 
 @NgModule({
@@ -12,11 +16,15 @@ import { ListSuggestionComponent } from './core/list-suggestion/list-suggestion.
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    NotfoundComponent,
     ListSuggestionComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule,
+    FormsModule, // IMPORTANT pour ngModel
+    CommonModule // IMPORTANT pour ngClass, date pipe, uppercase pipe
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/notfound/notfound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound', // Note: 'notfound' avec 'f' minuscule
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css']
+})
+export class NotfoundComponent { // Note: 'NotfoundComponent' avec 'f' minuscule
+  // ...
+}
